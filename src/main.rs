3bcf02b42423bd5afb134225a2753510166b3c22
Your Name <you@example.com>
2022-07-29 02:07:04 +0800

    add log

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,35 @@ struct UserPost {
      body: String
 }
 
+
+use log::info;
+
+fn init_logger() {
+    use chrono::Local;
+    use std::io::Write;
+
+    let env = env_logger::Env::default()
+        .filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
+    // 设置日志打印格式
+    env_logger::Builder::from_env(env)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} {} [{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.module_path().unwrap_or("<unnamed>"),
+                &record.args()
+            )
+        })
+        .init();
+    info!("env_logger initialized.");
+}
+
+
 #[get("/")]
 async fn hello() -> impl Responder {
-
+    info!("this is test");
     use diesel_demo::schema::posts::dsl::*;
     let conn = establish_connection();
     let results = posts.filter(published.eq(true))
@@ -60,6 +86,8 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    init_logger();
+    info!("hello world");
     HttpServer::new(|| {
         App::new()
             .service(hello)
